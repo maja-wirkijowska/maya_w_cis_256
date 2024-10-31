99869d782786be1524fe8ce1eacc35c8c0896998
maya <mwirkij@wgu.edu>
2024-10-31 15:27:53 -0700

    initial files commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # maya_w_cis_256
+
+## test.py
+A sample Python file.
+
+## README.md
+A sample README file.
+
+## index.html
+A sample HTML file.
---
+++ module_08_lab_part_b/test.py
@@ -0,0 +1,9 @@
+"""
+Maya Wirkijowska
+Module 08 Lab Assignment
+Part B
+
+Python file for sample GitHub repository.
+"""
+
+print("Hello, World!")
